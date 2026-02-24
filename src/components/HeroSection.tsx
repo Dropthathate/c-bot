16265f90ba7614049ad4aa204042a267543ad76d
Nate D Santos <161200005+Dropthathate@users.noreply.github.com>
2026-02-23 21:03:13 -0800

    aaliyah-logo.png

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -49,7 +49,7 @@ const HeroSection: React.FC = () => {
                 <div className="logo-container">
                     <img src="/images/ss.png" alt="SomaSync" style={{height: '50px'}} />
                     <div className="divider" style={{width:'1px', height:'40px', background:'#555', margin:'0 20px'}}></div>
-                    <img src="/images/newicon.jpg" alt="Aaliyah" style={{height: '50px', borderRadius:'50%'}} />
+                    <img src="/images/Aaliyah-logo.png" alt="Aaliyah" style={{height: '50px', borderRadius:'50%'}} />
                     <h2 style={{marginLeft:'10px'}}>ΛΛLIYΛH.IO</h2>
                 </div>
             </header>

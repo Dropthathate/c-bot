9387358651c7d084079e6ba17d280d7d34038f13
Nate D Santos <161200005+Dropthathate@users.noreply.github.com>
2026-03-15 16:40:03 -0700

    Add all pages

--- somasync/app/layout.tsx
+++ somasync/app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
